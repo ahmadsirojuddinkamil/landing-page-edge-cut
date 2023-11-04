9ea54fd7532a9dcf4ec9e1d5e1ffffcf73f387d8
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2023-11-04 18:36:53 +0700

    source code landing page edge cut

--- resources/views/comments/index.blade.php
+++
@@ -1,3 +0,0 @@
-<x-turbo-frame id="comments">
-    @each('comments/_comment', $comments, 'comment', 'posts/_empty')
-</x-turbo-frame>
--- resources/views/likes/_likes.blade.php
+++
@@ -1,4 +0,0 @@
-@include('likes/_like')
-
-<span id="@domid($post, 'likes_count')">{{ $post->likes_count }}</span>
-
